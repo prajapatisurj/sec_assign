646303e4d30e195a4dee37c1e7d4e4cab02090d3
prajapatisurj <47967303+prajapatisurj@users.noreply.github.com>
2021-10-27 22:25:29 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+
+const style = {
+  backgroundColor: "black",
+  color: "white",
+  padding: "4px 8px",
+  border: "none",
+  borderRadius: "4px",
+  display: "block",
+  marginBottom: "4px",
+  fontWeight: "bold",
+};
+
+const App = () => {
+  axios.defaults.baseURL = "https://reqres.in/api";
+
+  const getData = () => {
+    axios.get("/users").then(({ data: data }) => console.log(data));
+  };
+
+  const config = {
+    data: {
+      name: "John Doe",
+      job: "Junior Developer",
+    },
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+
+  const postData = () => {
+    axios
+      .post("/users", config)
+      .then((res) => console.log(res.data))
+      .catch((res) => console.log(res));
+  };
+
+  const updateData = async () => {
+    try {
+      const res = await axios.put("/2", {
+        name: "Jane Doe",
+        job: "Senior Developer",
+      });
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteData = () => {
+    axios.delete("/2").then((res) => console.log(res.status));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='grid'>
+        <button style={style} onClick={getData}>
+          Get
+        </button>
+        <button style={style} onClick={postData}>
+          Post
+        </button>
+        <button style={style} onClick={updateData}>
+          Update
+        </button>
+        <button style={style} onClick={deleteData}>
+          Delete
+        </button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
+
